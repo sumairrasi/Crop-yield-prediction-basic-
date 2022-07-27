d85b85db0c9720eed44928aab68947a3630d0120
Sumair Rasi <85817743+sumairrasi@users.noreply.github.com>
2022-07-27 06:56:26 -0700

    Update model.py

--- model.py
+++ model.py
@@ -3,11 +3,6 @@ import pickle
 from sklearn.model_selection import train_test_split
 data = pd.read_csv("labeldata.csv")
 
-#from sklearn.preprocessing import LabelEncoder
-#labelencoder=LabelEncoder()
-#Agri_data_Cols=data[['Area','Item_x']]
-#for i in Agri_data_Cols:
-#   data[i] = labelencoder.fit_transform(data[i])
 
 y = data['yield']
 X = data.drop(['yield','Domain Code', 'Domain_x',  'Element Code','Element_x',
@@ -17,7 +12,7 @@ X = data.drop(['yield','Domain Code', 'Domain_x',  'Element Code','Element_x',
 
 
 
-X_train, X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+X_train, X_test,y_train,y_test = train_test_split(X.values,y,test_size=0.2,random_state=42)
 from sklearn.ensemble import RandomForestRegressor
 rf_grid =RandomForestRegressor()
 rf_grid.fit(X_train,y_train)
